be8874bfaa4c26a287d09c3ee2a1897cd456a727
PhungTrinhUET <128596518+PhungTrinhUET@users.noreply.github.com>
2025-02-20 08:35:36 +0700

    Commit BaiTapPhanSoTuan3

---
+++ BaiTap2/src/main/java/PhanSo.java
@@ -0,0 +1,20 @@
+public class PhanSo {
+    private int numerator;
+    private int denominator;
+
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public void setNumerator(int numerator) {
+        this.numerator = numerator;
+    }
+
+    public int getDenominator() {
+        return denominator;
+    }
+
+    public void setDenominator(int denominator) {
+        this.denominator = denominator;
+    }
+}
