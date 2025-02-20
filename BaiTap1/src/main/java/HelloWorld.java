99761f0c54004a78f710b208319e3aa39dbea5f5
PhungTrinhUET <128596518+PhungTrinhUET@users.noreply.github.com>
2025-02-20 08:28:16 +0700

    Commit BaiTapHelloWorld

---
+++ BaiTap1/src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static String printHello() {
+        return "HelloWorld";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(printHello());
+    }
+}
